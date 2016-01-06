84e8ad699f9648770b9b070c6106800ad701c4a8
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-01-06 00:44:05 +0000

    Update server.py
    To add static file location dict etc

--- Pi/server.py
+++ Pi/server.py
@@ -1,9 +1,13 @@
+#Tornado elements
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
- 
+
+# utility libraries
+import os.path
+
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
  
