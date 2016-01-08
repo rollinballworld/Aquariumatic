afdf0b241d570899052cc6463006648aa5efd4ec
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-01-08 01:26:26 +0000

    Update server.py

--- Pi/server.py
+++ Pi/server.py
@@ -1,13 +1,9 @@
-#Tornado elements
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
-
-# utility libraries
-import os.path
-
+ 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
  
@@ -15,41 +11,53 @@ class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
-class PostHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('test.html')
+class TankHandler(tornado.web.RequestHandler):
+    def get(self, input):
+        self.render('tank.html')
+        
+    def post(self, input):
+        aquarium_id = input
+        heater = self.get_argument('Heating', '')
+        lighting = self.get_argument('Light', '')
+        mintemp_data = self.get_argument('MinTemp', '')
+        maxtemp_data = self.get_argument('MaxTemp', '')
+        
+        if heater == 'ON':
+            #turn relay on for heating on that particular tank
+            self.write('switching relay ON')
+            return
+        elif heater == 'OFF':
+            #turn relay off for heating on that particular tank
+            self.write('switching relay OFF')
+            return
+        else:
+            #No action Required
+            #self.write('parameter not defined for heating')
+            x=1
+            
+        if lighting == '1':
+            self.write('toggle FRONT light')
+            return
+        elif lighting == '2':
+            self.write('Toggle BACK light')
+            return
+        elif lighting == '3':
+            self.write('Toggle TOP Light')
+            return
+        else:
+            #No action Required
+            #self.write('parameter not defined for heating')
+            x=2
+        
+        login_response = 'Min temp threshold: ' + mintemp_data + '   Max temp threshold: ' + maxtemp_data
+        self.write('Aquarium Number:' + aquarium_id + " " + login_response)
 
-    def post(self):
-        say_data = self.get_argument('say', '')
-        to_data = self.get_argument('to', '')
-
-        #login_response = {
-        #    'say': say_data,
-        #    'to': to_data,
-        #    'msg': 'Data received;'
-        #}
-        login_response = 'say ' + say_data + ' to ' + to_data
-        self.write(login_response)
-
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print ('new connection')
-        self.write_message("connected")
- 
-    def on_message(self, message):
-        print ('message received %s' % message)
-        self.write_message('message received %s' % message)
- 
-    def on_close(self):
-        print ('connection closed')
- 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     app = tornado.web.Application(
         handlers=[
             (r"/", IndexHandler),
-            (r"/test", PostHandler),
-            (r"/ws", WebSocketHandler)
+            (r"/aquarium(\d+)", TankHandler),
         ]
     )
     httpServer = tornado.httpserver.HTTPServer(app)
