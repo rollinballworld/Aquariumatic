0040a8537845f260a23a3c4a618ee657c741d1ba
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-01-12 17:24:51 +0000

    Create functions.py

---
+++ Pi/functions.py
@@ -0,0 +1,16 @@
+#This will contain all functions required to send data to ad from the Arduinos attached via i2c.
+#
+#
+#
+#
+
+#def SlaveButtonSend(AquariumNumber, FunctionName, FunctionValue):
+#Send AquariumNumber to i2cList to have the i2c address returned
+#Send FunctionName & FunctionValue to the returned i2c Address
+
+#def SlaveParameterSend(AquariumNumber, FunctionName, MinValue, MaxValue):
+#Send AquariumNumber to i2cList to have the i2c address returned
+#Send FunctionName & Values to the returned i2c Address
+
+#def i2cList(AquariumNumber):
+#Contain a list of i2c addresses and return the appropriate address when provided with an Aquarium number
