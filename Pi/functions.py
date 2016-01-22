f411dd16e20b506679534f62514364fd8464d592
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-01-22 15:42:27 +0000

    Update functions.py
    Added Gmail code to test

--- Pi/functions.py
+++ Pi/functions.py
@@ -31,3 +31,28 @@ def i2cList(AquariumNumber):
   #TO WRITE
   ListedValue = 0x15
   return ListedValue
+
+def Gmail(recipient, subject, body):
+    import smtplib
+
+    gmail_user = "aquariumatic@gmail.com"
+    gmail_pwd = "Craigisprettyclever"
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
+  
