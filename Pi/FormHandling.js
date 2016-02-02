560ed346aa31d3960848b66f81d3b86473043073
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-02-02 15:33:55 +0000

    Create FormHandling.js

---
+++ Pi/FormHandling.js
@@ -0,0 +1,35 @@
+//FormHandling.js
+$(document).ready(function() {
+
+    // process the form
+    $('form').submit(function(event) {
+
+        // get the form data
+        // there are many ways to get this data using jQuery (you can use the class or id also)
+        var formData = {
+            'mintemp'              : $('input[name=MinTemp]').val(),
+            'maxtemp'             : $('input[name=MaxTemp]').val(),
+        };
+
+        // process the form
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : 'server.py', // the url where we want to POST
+            data        : formData, // our data object
+            dataType    : 'json', // what type of data do we expect back from the server
+                        encode          : true
+        })
+            // using the done promise callback
+            .done(function(data) {
+
+                // log data to the console so we can see
+                console.log(data); 
+
+                // here we will handle errors and validation messages
+            });
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
+
+});
