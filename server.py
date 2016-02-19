89d02b94f20e5bd3c02e42152c50ecc752b7d1e1
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-02-19 11:34:46 +0000

    Update server.py

--- server.py
+++ server.py
@@ -98,7 +98,9 @@ if __name__ == "__main__":
             (r"/aquarium(\d+)", TankHandler)],
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
-            js_path=os.path.join(template_path, "js"))
+            js_path=os.path.join(template_path, "js"),
+            css_path=os.path.join(template_path, "css"),
+            images_path=os.path.join(template_path, "images"))
 
     
     httpServer = tornado.httpserver.HTTPServer(app)
