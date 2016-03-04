4eb1ee1968b8780ef41e41789d464edfbb51567a
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-03-04 12:46:10 +0000

    Create aquarium.py

---
+++ aquarium.py
@@ -0,0 +1,20 @@
+#To replace the functions and data python scripts.
+#Create and aquarium class which gathers the current sonsor data from the tank's 
+#arduino (ID nubmer to be provided, i2c address to be found)
+#
+#add functions to deal with sending commands to the arduino, sending parameters, receiving alerts and receiving data.
+
+Class Aquarium(TankID):
+  #To fill out
+
+  def SendCommand:
+    pass
+  
+  def SendParameter:
+    pass
+    
+  def GetData:
+    pass
+  
+  def CheckAlerts:
+    pass
