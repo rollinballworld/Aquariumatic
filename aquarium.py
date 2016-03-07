d80ad3e8cbe33e2738f71c5ea21a31306d708810
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-03-07 17:13:13 +0000

    Update aquarium.py

--- aquarium.py
+++ aquarium.py
@@ -4,14 +4,30 @@
 #
 #add functions to deal with sending commands to the arduino, sending parameters, receiving alerts and receiving data.
 
-class Aquarium(TankID):
+class Aquarium(AquariumNumber):
   #To fill out
 
-  def SendCommand:
-    pass
-  
-  def SendParameter:
-    pass
+  def SendCommand(AquariumNumber, FunctionName, FunctionValue):
+    #Send AquariumNumber to i2cList to have the i2c address returned
+    AqAddress=i2cList(AquariumNumber)
+    #Send FunctionName & FunctionValue to the returned i2c Address
+    if FunctionName == 'test':
+      #Setup a test return value
+      print("Test Script receipt")
+    else:
+      #Run normal i2c send to - TO WRITE
+      pass
+    
+  def SendParameter(AquariumNumber, FunctionName, MinValue, MaxValue):
+    #Send AquariumNumber to i2cList to have the i2c address returned
+    AqAddress=i2cList(AquariumNumber)
+    #Send FunctionName & FunctionValue to the returned i2c Address
+    if FunctionName == 'test':
+      #Setup a test return value
+      print("Test Script receipt")
+    else:
+      #Run normal i2c send to - TO WRITE
+      pass
     
   def CurrentReadings:
     CurrentReadings[]
@@ -23,3 +39,34 @@ class Aquarium(TankID):
   
   def CheckAlerts:
     pass
+
+
+def i2cList(AquariumNumber):
+  #Contain a list of i2c addresses and return the appropriate address when provided with an Aquarium number
+  #TO WRITE
+  ListedValue = 0x15
+  return ListedValue
+
+def Gmail(recipient, subject, body):
+    import smtplib
+
+    gmail_user = "aquariumatic@gmail.com"
+    gmail_pwd = "pw"
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
