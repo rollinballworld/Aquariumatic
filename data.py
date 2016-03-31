03a61b4917faab3afd12237fb91a969acab4e993
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-03-31 11:01:44 +0100

    Update data.py

--- data.py
+++ data.py
@@ -3,11 +3,26 @@
 #
 #This will be scheduled to run on a Cron job every x minutes, and the server script can then retrieve values from the csv files.
 #Allows the page to use the values independently of Arduinos.
+from time import sleep
+import smbus
 
 def i2cList():
-  #compile a list of i2c addresses to query
-  return addresslist
-  
+  print("Scanning")
+    bus = smbus.SMBus(1) # 1 indicates /dev/i2c-1; use 0 if not working
+    found_slaves = []
+    for device in range(128):
+      try: #If you try to read and it answers, it's there
+         bus.read_byte(device)
+      except IOError: 
+         pass
+      else:
+         found_slaves.append(str(hex(device)))
+    device_count = len(found_slaves)
+    if device_count == 0:
+      print("No Aquariumatic slave devices found")
+      sleep(2)
+    else:
+      return found_slaves
 
 def getValues(address):
   # request values from i2c address provided and write it to csv file
