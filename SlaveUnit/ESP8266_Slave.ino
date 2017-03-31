a6c912808cd18d81d1539df29e4696098dcc26ff
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-03-31 16:31:21 +0100

    Update ESP8266_Slave.ino

--- SlaveUnit/ESP8266_Slave.ino
+++ SlaveUnit/ESP8266_Slave.ino
@@ -7,5 +7,8 @@ Forward string via websocket connection
 Check whether best to set up the websocket server on the Pi and have the ESP8266s connect to that, or 
 whether to have the ESP8266s as the Socket servers and have the pi operate as the client.
 
+For now have the server on the ESP8266 (plenty of examples) and add a button to the Pi's web page to 
+send a 'request' command, and receive the string returned.
+
 Check out these sources:
 https://github.com/morrissinger/ESP8266-Websocket/blob/master/examples/WebSocketClient_Demo/WebSocketClient_Demo.ino*/
