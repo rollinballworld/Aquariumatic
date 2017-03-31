4284057d71362cd3246fd3b81d1e4985a7144a83
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-03-31 16:24:19 +0100

    Create ESP8266_Slave.ino

---
+++ SlaveUnit/ESP8266_Slave.ino
@@ -0,0 +1,11 @@
+/*This sketch will:
+Connect to a wifi network on startup
+Start the serial connection to the Arduino
+Request a data string from the Arduino (in json format)
+Forward string via websocket connection
+
+Check whether best to set up the websocket server on the Pi and have the ESP8266s connect to that, or 
+whether to have the ESP8266s as the Socket servers and have the pi operate as the client.
+
+Check out these sources:
+https://github.com/morrissinger/ESP8266-Websocket/blob/master/examples/WebSocketClient_Demo/WebSocketClient_Demo.ino
